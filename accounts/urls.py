62a073e016eaefa7c1260ad5bc3462eef2861140
john-otienoh <johnteclaire@gmail.com>
2025-11-12 10:01:57 +0300

    Create password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('verify_otp/<uuid:user_id>/', views.verify_otp, name='verify_otp'),
     path('resend_otp/<uuid:user_id>/', views.resend_otp, name='resend_otp'),
+    path('forgot-password/', views.forgot_password, name='forgot-password'),
+    path('reset-password/<uidb64>/<token>/', views.reset_password, name='reset_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,13 @@ from django.contrib.auth import login,  logout
 from .forms import EmailAuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.core.mail import send_mail
 from django.utils import timezone
 from django.contrib import messages
-
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
 
 from .utils import generate_otp, send_otp, check_otp
 from .models import CustomUser
@@ -121,3 +125,58 @@ def logout_view(request):
 
 def home(request):
     return render(request, 'home.html')
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = CustomUser.objects.filter(email=email).first()
+        
+        if not user:
+            messages.error(request, "No account with that email found")
+            return redirect('forgot-password')
+        
+        if hasattr(user, 'is_email_verified') and not user.is_email_verified:
+            messages.error(request, 'Please verify your email before resetting your password.')
+            return redirect('forgot_password')
+
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        reset_link = request.build_absolute_uri(f'/account/reset-password/{uid}/{token}/')
+
+        send_mail(
+            'Password Reset Request',
+            f'Click the link to reset your password: {reset_link}',
+            settings.EMAIL_HOST_USER,
+            [email],
+            fail_silently=False,
+        )
+        messages.success(request, 'Password reset link sent to your email.')
+        return redirect('login')
+    return render(request, "forgot_password.html")
+
+
+def reset_password(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = CustomUser.objects.get(pk=uid)
+    except:
+        user = None
+    
+    if user and default_token_generator.check_token(user, token):
+        if request.method == 'POST':
+            password = request.POST.get('password')
+            password2 = request.POST.get('password2')
+
+            if password == password2:
+                user.set_password(password)
+                user.save()
+                messages.success(request, "'Password reset successful. You can now log in.")
+                return redirect('login')
+            else:
+                messages.error(request, "Passwords do not match.")
+        return render(request, 'reset_password.html')
+    else:
+        messages.error(request, "Invalid or expired reset link")
+        return redirect('forgot_password')
+    
